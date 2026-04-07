25e838c24505534598aa4ca4a109bd270953bf88
Joe Kaufeld <opensource@joekaufeld.com>
2026-04-06 23:34:53 -0400

    add more valid characters to yaml front matter regex and release 1.0.3

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,7 @@ joplin.plugins.register({
                 const errors = [];
 
                 // Success State: Valid Front Matter block at the beginning
-                const yamlFrontMatterRegex = /^-{3}\n(?:[a-zA-Z\s]+: ?(?:"?[\w,.\-+:\s?&_]+"?|[\d\-T:+]+|\[]| |(?:\s+- [a-z ]+)+|true|false|null|)\n)+(?:-{3}|\.{3})/;
+                const yamlFrontMatterRegex = /^-{3}\n(?:[a-zA-Z\s]+: ?(?:"?[\w,.'\-+*()@#$%^:\s!?&_]+"?|[\d\-T:+]+|\[]| |(?:\s+- [a-z ]+)+|true|false|null|)\n)+(?:-{3}|\.{3})/gm;
                 const tomlFrontMatterRegex = /^\+{3}\n(?:[a-zA-Z\s]+ = (?:[\d\-T:]+|false|'.*')\n|^\[.*]$)+\+{3}/gm;
 
                 if (body.startsWith('---')) {
